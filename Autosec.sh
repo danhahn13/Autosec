89a9a6f9ee0641f7c9834042b7a146c60e7b5b11
Daniel Hahn <danhahn15@gmail.com>
2022-07-16 13:18:10 -0400

    External domain enum update

--- Autosec.sh
+++ Autosec.sh
@@ -1,30 +1,51 @@
 #!/bin/bash
 
 echo "
-    ______                   ______                    
-   / ____/___ ________  __   / ____/___  __  ______ ___ 
-  / __/ / __ `/ ___/ / / /  / __/ / __ \/ / / / __ `__ \
- / /___/ /_/ (__  ) /_/ /  / /___/ / / / /_/ / / / / / /
-/_____/\__,_/____/\__, /  /_____/_/ /_/\__,_/_/ /_/ /_/ 
-                 /____/                                 
+    ___         __                      
+   /   | __  __/ /_____  ________  _____
+  / /| |/ / / / __/ __ \/ ___/ _ \/ ___/
+ / ___ / /_/ / /_/ /_/ (__  )  __/ /__  
+/_/  |_\__,_/\__/\____/____/\___/\___/  
 
-Usage: "
+ 
+Usage: 
+./Autosec -i 10.1.10.0/24"
 
-#if [ $# -gt 2 ]; then echo ""
 
 #subfinder command that gets all domain names
-echo "Finding all domains..."
-subfinder -d $1 -o domains.txt
-echo $1 >> domains.txt #throws the original domain back into the list
+echo "Finding all subdomains..."
+subfinder -d $1 -o subdomains.txt
+
+#Regex to extract only third-level domain names with no duplicates.
+cat subdomains.txt | grep -Po "(\w+\.\w+\.\w+)$" | sort -u >> thirdlevel-subdomains.txt
+
+#if statement to make a directory only if it does not exist.
+if [ ! -d "third" ]; then
+    mkdir third
+fi
+
+#Re-run subfinder on each third level domain to get a complete list of subdomains
+for x in $(cat thirdlevel-subdomains.txt);
+    do subfinder -d $x -o third/$x.txt;
+    cat third/$x.txt | sort -u >> subdomains.txt;
+done
+
+#throws the original domain back into the list
+echo $1 >> subdomains.txt 
+
+#clear extra files
+rm thirdlevel-subdomains.txt && rm -r third
 
 #Finding which domains are alive
 echo "Extracting only live domains..."
-cat domains.txt | httprobe -s -p https:443 | sed 's/https\?:\/\///' | tr -d ":443" > livehosts.txt
+cat subdomains.txt | sort -u |httprobe -s -p https:443 | sed 's/https\?:\/\///' | tr -d ":443" > live-subdomains.txt
 
-#Performing a port scan on all live hosts and saving into a directory
-echo "Performing a port scan..."
+#Performing a TCP and UDP port scan on all live hosts and saving into a directory
+echo "Performing a TCP port scan..."
 if [ ! -d "scannedhosts" ]; then
 	mkdir scannedhosts
 fi
+nmap -Pn -p- -iL live-subdomains.txt -oN scannedhosts/TCP.txt
 
-nmap -Pn -p- -iL livehosts.txt -oN scannedhosts/scans.txt
+echo "Performing a UDP port scan..."
+nmap -Pn -sU --top-ports 100 -iL live-subdomains.txt -oN scannedhosts/UDP.txt
