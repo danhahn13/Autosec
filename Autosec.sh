3d8ecea8ea6afddd0d519ab70b5781698f6622f7
Daniel Hahn <danhahn15@gmail.com>
2022-08-13 08:33:59 -0400

    Updates post testing

--- Autosec.sh
+++ Autosec.sh
@@ -13,13 +13,13 @@ echo "
 "
 
 #Choice of functionality
-OPTION=$(zenity --list --column="Options" --title="Choose an option" "External enumeration" "Internal enumeration" "Email filtering test")
+OPTION=$(zenity --list --column="Options" --title="Choose an option" "External enumeration" "Internal enumeration" "Email filtering test" --width=600 --height=400)
 
 #If statement for choice of functionality
 if [[ $OPTION == "External enumeration" ]]; then
 
     #User enters domain
-    DOMAIN=$(zenity --entry --text "Enter your domain" --title "External Enumeration" --entry-text="Example: google.com")
+    DOMAIN=$(zenity --entry --text "Enter your domain" --title "External Enumeration" --entry-text="Example: google.com" --width=600 --height=400)
 
     function external () {
 
@@ -28,24 +28,19 @@ if [[ $OPTION == "External enumeration" ]]; then
         echo "25"
 
         #subfinder command that gets all domain names
-        subfinder -d $DOMAIN -o subdomains.txt
+        subfinder -d $DOMAIN -o $DOMAIN/subdomains.txt
 
         #Regex to extract only third-level domain names with no duplicates.
-        cat subdomains.txt | grep -Po "(\w+\.\w+\.\w+)$" | sort -u >> thirdlevel-subdomains.txt
-
-        #if statement to make a directory only if it does not exist.
-        if [ ! -d "third" ]; then
-            mkdir third
-        fi
+        cat $DOMAIN/subdomains.txt | grep -Po "(\w+\.\w+\.\w+)$" | sort -u >> thirdlevel-subdomains.txt
 
         #Re-run subfinder on each third level domain to get a complete list of subdomains
         for x in $(cat thirdlevel-subdomains.txt);
             do subfinder -d $x -o third/$x.txt;
-            cat third/$x.txt | sort -u >> subdomains.txt;
+            cat third/$x.txt | sort -u >> $DOMAIN/subdomains.txt;
         done
 
         #throws the original domain back into the list
-        echo $DOMAIN >> subdomains.txt 
+        echo $DOMAIN >> $DOMAIN/subdomains.txt 
 
         #clear extra files
         rm thirdlevel-subdomains.txt && rm -r third
@@ -53,65 +48,64 @@ if [[ $OPTION == "External enumeration" ]]; then
         #Finding which domains are alive
         echo "# Extracting only live domains..."
         echo "50"
-        cat subdomains.txt | sort -u |httprobe -s -p https:443 | sed 's/https\?:\/\///' | tr -d ":443" > live-subdomains.txt
+
+        cat $DOMAIN/subdomains.txt | sort -u |httprobe -s -p https:443 | sed 's/https\?:\/\///' | tr -d ":443" > $DOMAIN/live-subdomains.txt
 
         #Performing a TCP and UDP port scan on all live hosts and saving into a directory
         echo "# Performing a TCP port scan..."
         echo "75"
-        if [ ! -d "scannedhosts" ]; then
-            mkdir scannedhosts
-        fi
-        nmap -Pn --top-ports 10 -iL live-subdomains.txt -oN scannedhosts/TCP.txt
+        
+        nmap -Pn --top-ports 10 -iL $DOMAIN/live-subdomains.txt -oN $DOMAIN/scannedhosts/TCP.txt
 
         echo "# Performing a UDP port scan..."
         echo "100"
-        nmap -Pn -sU --top-ports 10 -iL live-subdomains.txt -oN scannedhosts/UDP.txt
+
+        nmap -Pn -sU --top-ports 10 -iL $DOMAIN/live-subdomains.txt -oN $DOMAIN/scannedhosts/UDP.txt
     }
     #progress bar
-    external | zenity --progress --title "External Enumeration" --auto-close
+    external | zenity --progress --title "External Enumeration" --auto-close --width=600 --height=400
 
 
 
 elif [[ $OPTION == "Internal enumeration" ]]; then
 
     #User enters IP range
-    IP=$(zenity --entry --text "Enter your IP range" --title "Internal Enumeration" --entry-text="Example: 10.1.10.0/24")
+    IP=$(zenity --entry --text "Enter your IP range" --title "Internal Enumeration" --entry-text="Example: 10.1.10.0/24" --width=600 --height=400)
 
     function internal () {
 
         #mapping the network
         echo "# Mapping the network..."
         echo "33"
+
         nmap -sn $IP -oN livehosts.txt
 
         #take out everythhing but the ip addresses so we can perform further scanning
-        cat livehosts.txt | awk '/is up/ {print up}; {gsub (/\(|\)/,""); up =$NF}' > livehosts-list.txt
+        cat livehosts.txt | awk '/is up/ {print up}; {gsub (/\(|\)/,""); up =$NF}' > $IP/livehosts-list.txt
 
         #performing a TCP and UDP port scan on live hosts
-        if [ ! -d "portscans" ]; then
-            mkdir portscans
-        fi
-
         echo "# Performing a TCP port scan..."
         echo "66"
-        nmap -Pn --top-ports 10 -iL livehosts-list.txt -oN portscans/TCP.txt
+
+        nmap -Pn --top-ports 10 -iL $IP/livehosts-list.txt -oN $IP/portscans/TCP.txt
 
         echo "# Performing a UDP port scan..."
         echo "100"
-        nmap -Pn -sU --top-ports 10 -iL livehosts-list.txt -oN portscans/UDP.txt
+
+        nmap -Pn -sU --top-ports 10 -iL $IP/livehosts-list.txt -oN $IP/portscans/UDP.txt
     }
     #Progress bar
-    internal | zenity --progress --title "Internal Enumeration" --auto-close
+    internal | zenity --progress --title "Internal Enumeration" --auto-close --width=600 --height=400
 
 elif [[ $OPTION == "Email filtering test" ]]; then
 
-    #modify the details below
-    TO=$(zenity --entry --text "Enter the target email" --title "Email Filtering Test" --entry-text="Example: johndoe@tesla.com")
-    FROM=$(zenity --entry --text "Enter the sender email" --title "Email Filtering Test" --entry-text="Example: janedoe@tesla.com")
-    SERVER=$(zenity --entry --text "Enter the SMTP address and port" --title "Email Filtering Test" --entry-text="Example: xxx.outlook.com:25")
+    #User enters email detaisl
+    TO=$(zenity --entry --text "Enter the target email" --title "Email Filtering Test" --entry-text="Example: johndoe@tesla.com" --width=600 --height=400)
+    FROM=$(zenity --entry --text "Enter the sender email" --title "Email Filtering Test" --entry-text="Example: janedoe@tesla.com" --width=600 --height=400)
+    SERVER=$(zenity --entry --text "Enter the SMTP address and port" --title "Email Filtering Test" --entry-text="Example: xxx.outlook.com:25" --width=600 --height=400)
 
     #Spoofed address
-    SPOOFINTERNAL=$(zenity --entry --text "Spoof an internal address" --title "Email Filtering Test" --entry-text="Example: janedoe@tesla.com")
+    SPOOFINTERNAL=$(zenity --entry --text "Spoof an internal address" --title "Email Filtering Test" --entry-text="Example: janedoe@tesla.com" --width=600 --height=400)
 
     #Spoof external email with softfail(~all) and hardfail(-all).
     SPOOFSPF_SOFT=ceo\@dell.com
@@ -192,10 +186,10 @@ elif [[ $OPTION == "Email filtering test" ]]; then
         -m "This is the sixth test - embedded URL https://www.google.com"
     }
     #Progress bar
-    email | zenity --progress --title "Email filtering test" --auto-close
+    email | zenity --progress --title "Email filtering test" --auto-close --width=600 --height=400
        
 
 else
 
-    zenity --warning --text="Please try again and select an option"
+    zenity --warning --text="Please try again and select an option" --width=600 --height=400
 fi
